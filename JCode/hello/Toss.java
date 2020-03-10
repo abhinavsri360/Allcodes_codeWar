fc931dded2eab58b1d8f762530cb7838e240a457
abhinavsri360 <abhinav.022000@gmail.com>
2020-03-11 00:39:36 +0530

    Toss

---
+++ JCode/hello/Toss.java
@@ -0,0 +1,8 @@
+public class Toss {
+    public static void main(String[] args) {
+        if (Math.random() < 0.5)
+            System.out.println("Heads");
+        else
+            System.out.println("Tails");
+    }
+}
