ac0df515e0601873c8a3821243b55866dd355823
abhinavsri360 <abhinav.022000@gmail.com>
2020-03-12 04:34:21 +0530

    Birthday done right

--- JCode/hello/Birthday.java
+++ JCode/hello/Birthday.java
@@ -2,25 +2,32 @@ public class Birthday {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trial = Integer.parseInt(args[1]);
-        int c = 1;
-        double b;
-        double[][] r;
+        int i, t = trial;
+        double j;
+        int[] b = new int[n];
         int[] sum = new int[n];
-        sum[0] = 0;
-        r = new double[trial][n];
-        trial -= 1;
-        while (trial != -1) {
-            for (int i = 0; i < n; i++) {
-                b = Math.random() * n;
-                r[trial - 1][i] = b;
-                for (int j = 0; j < i; j++) {
-                    if (r[trial][j] == r[trial][i]) {
-                        c++;
-                        break;
-                    }
-                }
+        int[] s = new int[n];
+
+        while (trial != 0) {
+            for (i = 0; i < n; i++) {
+                b[i] = (int) (Math.random() * n);
+            }
+            for (i = 1; i < n; i++) {
+                if (b[i] == b[i - 1])
+                    sum[i] += 1;
             }
             trial -= 1;
+        }
+        for (i = 1; i < n; i++) {
+            s[i] = sum[i];
+            sum[i] += sum[i - 1];
+        }
+        for (i = 1; i < n; i++) {
+            j = (double) sum[i] / t;
+            if (j * 100 >= 51)
+                break;
+            else
+                System.out.println(i + "\t" + s[i] + "\t" + j);
         }
     }
 }
