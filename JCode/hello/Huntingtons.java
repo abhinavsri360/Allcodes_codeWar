0ca0ed4d3261f9cad21031b2b89821ee69c19744
abhinavsri360 <abhinav.022000@gmail.com>
2020-03-20 18:34:43 +0530

    baboon did the job
    :100:

--- JCode/hello/Huntingtons.java
+++ JCode/hello/Huntingtons.java
@@ -4,20 +4,22 @@ public class Huntingtons {
     public static void main(String[] args) {
         String s = args[0];
         File file = new File(s);
-        In b = new In(file.getAbsolutePath());
+        In b = new In("./" + file);
         s = b.readAll();
-        int d = maxRepeats(s);
+        int d = maxRepeats(removeWhitespace(s));
         System.out.println("max repeats = " + d);
-        System.out.println(diaganose(d));
+        System.out.println(diagnose(d));
     }
 
-    public static String diaganose(int maxRepeats) {
+    public static String diagnose(int maxRepeats) {
         if (maxRepeats < 10 || maxRepeats > 180)
             return "not human";
+        else if (maxRepeats < 36)
+            return "normal";
         else if (maxRepeats < 40)
-            return "no Huntigton's";
+            return "high risk";
         else
-            return "Huntigton's";
+            return "Huntington's";
     }
 
     public static String removeWhitespace(String s) {
@@ -25,20 +27,23 @@ public class Huntingtons {
     }
 
     public static int maxRepeats(String dna) {
-        String s = removeWhitespace(dna);
-        int c = 0, max = 0;
-        for (int i = 0; i < s.length() - 3; i++) {
-            while (s.charAt(i + 3) != 'T') {
-                if (s.substring(i, i + 3).equals("CAG")) {
+        int c = 0, max = 0, j;
+        for (int i = 0; i < dna.length() - 3; i++) {
+            j = i;
+            while (dna.charAt(j + 3) != 'T' || dna.substring(j, j + 3).equals("CAG")) {
+                if (dna.substring(j, j + 3).equals("CAG")) {
                     c++;
-                    i += 3;
+                    j += 3;
+                    if (j > dna.length() - 4 && dna.substring(j, j + 3).equals("CAG")) {
+                        c++;
+                        break;
+                    }
                 } else
                     break;
             }
-            if (++c > max)
+            if (c > max)
                 max = c;
             c = 0;
-            i += 3;
         }
         return max;
     }
--- JCode/hello/KernelFilter.java
+++ JCode/hello/KernelFilter.java
@@ -1,34 +1,43 @@
+import java.awt.*;
+
 public class KernelFilter {
     public static void main(String[] args) {
         Picture p = new Picture(args[0]);
         Picture q = gaussian(p);
-        Picture r = laplacian(p);
-        r.show();
+        q.show();
+        q = laplacian(p);
+        q.show();
+        q = sharpen(p);
+        q.show();
+        q = emboss(p);
+        q.show();
+        q = motionBlur(p);
+        q.show();
     }
 
     public static Picture gaussian(Picture picture) {
-        Picture q = picture;
+        Picture q = new Picture(picture);
         int[][] r = new int[picture.width()][picture.height()];
         int[][] g = new int[picture.width()][picture.height()];
         int[][] b = new int[picture.width()][picture.height()];
         int[][] k = {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}};
 
-        for (int i = 0; i < picture.width(); i++) {
-            for (int j = 0; j < picture.height(); j++) {
-                Color pic = picture.get(i, j);
+        for (int i = 0; i < q.width(); i++) {
+            for (int j = 0; j < q.height(); j++) {
+                Color pic = q.get(i, j);
                 r[i][j] = pic.getRed();
                 b[i][j] = pic.getBlue();
                 g[i][j] = pic.getGreen();
             }
         }
-        for (int i = 1; i < picture.width() - 1; i++) {
-            for (int j = 1; j < picture.height() - 1; j++) {
+        for (int i = 1; i < q.width() - 1; i++) {
+            for (int j = 1; j < q.height() - 1; j++) {
                 int red = r[i - 1][j - 1] * k[0][0] + r[i - 1][j] * k[0][1] + r[i - 1][j + 1] * k[0][2] + r[i][j - 1] * k[1][0] + r[i][j] * k[1][1] + r[i][j + 1] * k[1][2] + r[i + 1][j - 1] * k[2][0] + r[i + 1][j] * k[2][1] + r[i + 1][j + 1] * k[2][2];
                 int green = g[i - 1][j - 1] * k[0][0] + g[i - 1][j] * k[0][1] + g[i - 1][j + 1] * k[0][2] + g[i][j - 1] * k[1][0] + g[i][j] * k[1][1] + g[i][j + 1] * k[1][2] + g[i + 1][j - 1] * k[2][0] + g[i + 1][j] * k[2][1] + g[i + 1][j + 1] * k[2][2];
                 int blue = b[i - 1][j - 1] * k[0][0] + b[i - 1][j] * k[0][1] + b[i - 1][j + 1] * k[0][2] + b[i][j - 1] * k[1][0] + b[i][j] * k[1][1] + b[i][j + 1] * k[1][2] + b[i + 1][j - 1] * k[2][0] + b[i + 1][j] * k[2][1] + b[i + 1][j + 1] * k[2][2];
-                red = red / 16;
-                green = green / 16;
-                blue = blue / 16;
+                red = (int) Math.floor((double) red / 16.0);
+                green = (int) Math.floor((double) green / 16.0);
+                blue = (int) Math.floor((double) blue / 16.0);
                 if (red < 0)
                     red = 0;
                 else if (red > 255)
@@ -49,7 +58,7 @@ public class KernelFilter {
     }
 
     public static Picture laplacian(Picture picture) {
-        Picture q = picture;
+        Picture q = new Picture(picture);
         int[][] r = new int[picture.width()][picture.height()];
         int[][] g = new int[picture.width()][picture.height()];
         int[][] b = new int[picture.width()][picture.height()];
@@ -57,7 +66,7 @@ public class KernelFilter {
 
         for (int i = 0; i < picture.width(); i++) {
             for (int j = 0; j < picture.height(); j++) {
-                Color pic = picture.get(i, j);
+                Color pic = q.get(i, j);
                 r[i][j] = pic.getRed();
                 b[i][j] = pic.getBlue();
                 g[i][j] = pic.getGreen();
@@ -88,7 +97,7 @@ public class KernelFilter {
     }
 
     public static Picture sharpen(Picture picture) {
-        Picture q = picture;
+        Picture q = new Picture(picture);
         int[][] r = new int[picture.width()][picture.height()];
         int[][] g = new int[picture.width()][picture.height()];
         int[][] b = new int[picture.width()][picture.height()];
@@ -96,7 +105,7 @@ public class KernelFilter {
 
         for (int i = 0; i < picture.width(); i++) {
             for (int j = 0; j < picture.height(); j++) {
-                Color pic = picture.get(i, j);
+                Color pic = q.get(i, j);
                 r[i][j] = pic.getRed();
                 b[i][j] = pic.getBlue();
                 g[i][j] = pic.getGreen();
@@ -127,7 +136,7 @@ public class KernelFilter {
     }
 
     public static Picture emboss(Picture picture) {
-        Picture q = picture;
+        Picture q = new Picture(picture);
         int[][] r = new int[picture.width()][picture.height()];
         int[][] g = new int[picture.width()][picture.height()];
         int[][] b = new int[picture.width()][picture.height()];
@@ -135,7 +144,7 @@ public class KernelFilter {
 
         for (int i = 0; i < picture.width(); i++) {
             for (int j = 0; j < picture.height(); j++) {
-                Color pic = picture.get(i, j);
+                Color pic = q.get(i, j);
                 r[i][j] = pic.getRed();
                 b[i][j] = pic.getBlue();
                 g[i][j] = pic.getGreen();
@@ -166,7 +175,7 @@ public class KernelFilter {
     }
 
     public static Picture motionBlur(Picture picture) {
-        Picture q = picture;
+        Picture q = new Picture(picture);
         int[][] r = new int[picture.width()][picture.height()];
         int[][] g = new int[picture.width()][picture.height()];
         int[][] b = new int[picture.width()][picture.height()];
@@ -174,7 +183,7 @@ public class KernelFilter {
 
         for (int i = 0; i < picture.width(); i++) {
             for (int j = 0; j < picture.height(); j++) {
-                Color pic = picture.get(i, j);
+                Color pic = q.get(i, j);
                 r[i][j] = pic.getRed();
                 b[i][j] = pic.getBlue();
                 g[i][j] = pic.getGreen();
