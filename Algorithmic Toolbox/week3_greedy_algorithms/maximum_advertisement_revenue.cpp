aa030d82134f76610d1b5f224424a02a0131276e
abhinavsri360 <abhinav.022000@gmail.com>
2020-07-29 21:32:03 +0530

    advertise

--- Algorithmic Toolbox/week3_greedy_algorithms/maximum_advertisement_revenue.cpp
+++ Algorithmic Toolbox/week3_greedy_algorithms/maximum_advertisement_revenue.cpp
@@ -1,20 +1,34 @@
 #include <algorithm>
-#include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
 using namespace std;
 
 
 long long max_dot_product(vector<int> a, vector<int> b) {
-  // write your code here
+
+  int at,bt;
+  int n=a.size();
   long long result = 0;
-  for (size_t i = 0; i < a.size(); i++) {
-    result += ((long long) a[i]) * b[i];
+
+  //cout<<"reached"<<endl;
+  while(n--){
+    at=max_element(a.begin(), a.end()) - a.begin();
+    bt=max_element(b.begin(), b.end()) - b.begin();
+
+    result += ((long long) a[at]) * b[bt];
+    a[at]=INT_MIN;
+    b[bt]=INT_MIN;
   }
+
+  /*for (size_t i = 0; i < a.size(); i++) {
+    result += ((long long) a[i]) * b[i];
+  }*/
   return result;
 }
 
 int main() {
   size_t n;
+  cin>>n;
   
   vector<int> a(n), b(n);
   for (size_t i = 0; i < n; i++) {
@@ -23,6 +37,8 @@ int main() {
   for (size_t i = 0; i < n; i++) {
     cin>>b.at(i);
   }
+
+  cout<<max_dot_product(a,b)<<endl;
 
   return 0;
 }
